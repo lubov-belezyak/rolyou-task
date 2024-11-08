a207fd1db6814e24a9202a4c089209f50ba1d4d8
belezyak <LubovBelezyak@yandex.ru>
2024-11-08 14:28:25 +0500

    feat(Route): routes

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Src\Controllers\UserController;
+use Src\Services\Router;
+
+$router = new Router();
+
+$router->get('/get', 'UserController@index');
+$router->post('/create', 'UserController@store');
+$router->get('/get/{id}', 'UserController@show');
+$router->put('/update/{id}', 'UserController@update');
+$router->delete('/delete/{id}', 'UserController@delete');
+
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+$router->resolve($uri, $method);
